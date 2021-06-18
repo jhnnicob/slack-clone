4ed65e4d1af064ae21d674683b2b48feb3f10d8f
Nico Molon <johnnicomolon@yahoo.com>
2021-06-18 12:51:06 +0800

    Deleted header class

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Header from './Header'
+import Sidebar from './Sidebar'
+import Chat from './Chat'
+import {
+    BrowserRouter as Router, 
+    Switch,
+    Route
+  } from 'react-router-dom';
 
 function App() {
+  //BEM naming convention
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+      <Header />
+        <div className="app_body">
+          <Sidebar />
+
+          <Switch>
+            <Route path="/room/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h1>Welcome</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
